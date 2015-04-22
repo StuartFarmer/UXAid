aba2194cf2894c1c1b93cde62f818299bf4eb360
Stuart Farmer <email@stuartfarmer.me>
2015-04-22 17:19:54 -0400

    initial commit

---
+++ UXAid/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UXAid
+//
+//  Created by Stuart Farmer on 4/22/15.
+//  Copyright (c) 2015 Stuart Farmer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UXAid/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UXAid
+//
+//  Created by Stuart Farmer on 4/22/15.
+//  Copyright (c) 2015 Stuart Farmer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
